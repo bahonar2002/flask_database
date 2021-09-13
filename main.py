c4f27cda269fd3ebb8341706dadf918b9f82f6e2
aminb2002 <bahonar2002@gmail.com>
2021-09-12 20:57:42 -0400

    make the file

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask , request
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+db.create_all() 
+
+
+@app.route("/signup/" , method = ["POST"])
+def signUp():
+    username = request.form["username"]
+    password = request.form["password"]
+    user = User(username, password)
+    db.session.add(user)
+    db.session.commit()
+    return {"username": username , "password":password}
+
+@app.route("/show/<int:id>/")
+def show(id):
+    try:
+        user = User.query.get(id=id)
+        return {"username": user.username , "password":user.password}
+    except:
+        return "!!!!!"
+    
+app.run()
